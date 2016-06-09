625b488cd124f3c8b60b021c8c1591b1b83b7f20
zhengzhang <1083431279@qq.com>
2016-06-09 20:02:14 +0800

    test

--- wiseTMan/src/java/web/Util/CellType2.java
+++ wiseTMan/src/java/web/Util/CellType2.java
@@ -4,19 +4,19 @@ import org.apache.poi.ss.usermodel.*;
  * Created by pc-1 on 2016/6/2.
  */
 public enum CellType2 {
-    BLANK{
-        Object apply(Cell cell){return "";};
-    }, BOOLEAN{
-        Object apply(Cell cell){return cell.getBooleanCellValue();}
-    },ERROR{
-        Object apply(Cell cell){return "";};
-    },FORMULA{
-        Object apply(Cell cell){return cell.getArrayFormulaRange();};
-    },NUMERIC{
-        Object apply(Cell cell){return cell.getNumericCellValue();};
-    },STRING{
-        Object apply(Cell cell){return cell.getStringCellValue();};
-    };
-
-    abstract Object apply(Cell cell);
+//    BLANK{
+//        Object apply(Cell cell){return "";};
+//    }, BOOLEAN{
+//        Object apply(Cell cell){return cell.getBooleanCellValue();}
+//    },ERROR{
+//        Object apply(Cell cell){return "";};
+//    },FORMULA{
+//        Object apply(Cell cell){return cell.getArrayFormulaRange();};
+//    },NUMERIC{
+//        Object apply(Cell cell){return cell.getNumericCellValue();};
+//    },STRING{
+//        Object apply(Cell cell){return cell.getStringCellValue();};
+//    };
+//
+//    abstract Object apply(Cell cell);
 }
